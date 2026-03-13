31588b781b0980055fbcd55d97178fb978039d63
komuta0420 <komuta@bionicm.com>
2026-03-14 08:26:12 +0900

    AI検索にGemini使えるように

--- ai.js
+++ ai.js
@@ -7,9 +7,10 @@ const AI = (() => {
   // ────────────────────────────────────────
   const GEMINI = {
     API_BASE: 'https://generativelanguage.googleapis.com/v1beta',
-    // MODEL: 'gemini-2.5-flash',
-    // MODEL: 'gemini-3-flash-preview',
-    MODEL: 'gemini-3.1-flash-lite-preview',
+    MODEL: 'gemini-2.5-flash', // 20RPD, 検索は無料だと500RPD(先に20RPD制限かかる)
+    // MODEL: 'gemini-2.5-flash-lite',  // 20RPD, 検索は無料だと500RPD(先に20RPD制限かかる)
+    // MODEL: 'gemini-3-flash-preview', // 無料だと検索機能✕　
+    // MODEL: 'gemini-3.1-flash-lite-previe', // 無料だと検索機能✕　https://ai.google.dev/gemini-api/docs/pricing?hl=ja
 
     // 通常リクエスト（画像解析用、JSON強制モード）
     async request(prompt, imageBase64 = null) {
